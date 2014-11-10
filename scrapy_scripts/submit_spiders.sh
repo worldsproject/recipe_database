efa841ae3dd07d1f92ff79f0dd870ae07b24c04b
Tim Butram <tbutram@worldsproject.org>
2014-11-09 23:17:04 -0500

    Simple script to mass submit spiders.

---
+++ scrapy_scripts/submit_spiders.sh
@@ -0,0 +1,7 @@
+curl http://worldsproject-spider.cloudapp.net:6800/schedule.json -d project=default -d spider=allrecipes.com
+curl http://worldsproject-spider.cloudapp.net:6800/schedule.json -d project=default -d spider=www.bettycrocker.com
+curl http://worldsproject-spider.cloudapp.net:6800/schedule.json -d project=default -d spider=www.bhg.com        
+curl http://worldsproject-spider.cloudapp.net:6800/schedule.json -d project=default -d spider=www.campbellskitchen.com
+curl http://worldsproject-spider.cloudapp.net:6800/schedule.json -d project=default -d spider=www.eaglebrand.com
+curl http://worldsproject-spider.cloudapp.net:6800/schedule.json -d project=default -d spider=www.food.com
+curl http://worldsproject-spider.cloudapp.net:6800/schedule.json -d project=default -d spider=www.hersheys.com
