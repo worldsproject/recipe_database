fae14afb6b7ca2dcd51c191559ffd1b20caaaed7
Tim Butram <tbutram@worldsproject.org>
2015-01-23 11:49:26 -0500

    Fixing pylint warnings.

--- app/recipe_api.py
+++ app/recipe_api.py
@@ -8,12 +8,12 @@ from sqlalchemy_utils.expressions import (
 )
 
 
-"""
-Checks if a given user (based on their api key) is able to use the API,
-(they have free or paid credits)
-If a key given is a master key (from the config) always returns true.
-"""
 def user_able(user_key):
+	"""
+	Checks if a given user (based on their api key) is able to use the API,
+	(they have free or paid credits)
+	If a key given is a master key (from the config) always returns true.
+	"""
 	if user_key == app.config['API_KEY']:
 		return True
 
@@ -32,21 +32,21 @@ def user_able(user_key):
 
 	return False
 
-"""
-Given a recipe id, turns it into a json string. If admin is true, 
-returns the ingredient id as well.
-"""
 def json_recipe(recipe, admin=False):
-	rv = {}
+	"""
+	Given a recipe, turns it into a json string. If admin is true,
+	returns the ingredient id and admin key as well.
+	"""
+	returned_json = {}
 
-	rv['name'] = recipe.name
-	rv['directions'] = recipe.directions
-	rv['description'] = recipe.description
-	rv['prep_time'] = recipe.prep_time
-	rv['cook_time'] = recipe.cook_time
-	rv['image'] = recipe.image
-	rv['id'] = recipe.id
-	rv['origin'] = recipe.credit
+	returned_json['name'] = recipe.name
+	returned_json['directions'] = recipe.directions
+	returned_json['description'] = recipe.description
+	returned_json['prep_time'] = recipe.prep_time
+	returned_json['cook_time'] = recipe.cook_time
+	returned_json['image'] = recipe.image
+	returned_json['id'] = recipe.id
+	returned_json['origin'] = recipe.credit
 	i = []
 
 	for ing in recipe.ingredients:
@@ -64,15 +64,17 @@ def json_recipe(recipe, admin=False):
 
 		i.append(ings)
 
-	rv['ingredients'] = i
+	returned_json['ingredients'] = i
 
 	if admin:
-		rv['key'] = app.config['API_KEY']
+		returned_json['key'] = app.config['API_KEY']
 
-	return rv
+	return returned_json
 
 def get_unknown():
-	unknown = db.session.query(models.Ingredient_Name).filter(models.Ingredient_Name.name == 'unknown')
+	""" Gets the placeholder ingredient name """
+	unknown = db.session.query(models.Ingredient_Name) \
+	            .filter(models.Ingredient_Name.name == 'unknown')
 
 	if unknown.count() < 1:
 		ingn = models.Ingredient_Name(name='unknown')
@@ -84,12 +86,16 @@ def get_unknown():
 		return unknown.first().id
 
 def add_ingredient_name(name):
+	""" Adds a given name to ingredient_name if it does not exist.
+	If given an empty string, returns the unknown name.
+	"""
 	if len(name) < 1:
 		return get_unknown()
 
 	name = name.lower()
 
-	ing = db.session.query(models.Ingredient_Name).filter(models.Ingredient_Name.name == name)
+	ing = db.session.query(models.Ingredient_Name) \
+	        .filter(models.Ingredient_Name.name == name)
 
 	if ing.count() < 1:
 		new_ing = models.Ingredient_Name(name=name)
@@ -99,33 +105,42 @@ def add_ingredient_name(name):
 	else:
 		return ing.first().id
 
-"""
-Gets the recipe by given id.
-"""
-def get_recipe(recipe_id):
-	return db.session.query(models.Recipe).filter(models.Recipe.id == recipe_id).first()
 
-"""
-Allows admin access. If the key does not match, will immediatly abort.
-Otherwise nothing happens and can continue as if you have admin priv.
-"""
-def admin(key):
+def get_recipe(recipe_id):
+	"""
+	Gets the recipe by given id.
+	"""
+	return db.session.query(models.Recipe) \
+	         .filter(models.Recipe.id == recipe_id).first()
+
+
+def is_admin(key):
+	""" Allows admin access. If the key does not match, will immediatly abort.
+	Otherwise nothing happens and can continue as if you have admin priv.
+	"""
 	if key != app.config['API_KEY']:
 		abort(403)
 
 
 class IngredientAPI(Resource):
+	""" Returns a list of recipes
+	that either have or do not have the given ingredients.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
-		self.reqparse.add_argument('with', type=str, help='Ingredients that you require in your recipe.', action='append')
-		self.reqparse.add_argument('without', type=str, help='Ingredients that cannot be in your recipe.', action='append')
-		self.reqparse.add_argument('key', required=True, type=str, help="Your API key is required.")
+		self.reqparse.add_argument('with', type=str, \
+			help='Ingredients that you require in your recipe.', action='append')
+		self.reqparse.add_argument('without', type=str, \
+			help='Ingredients that cannot be in your recipe.', action='append')
+		self.reqparse.add_argument('key', required=True, type=str, \
+			help="Your API key is required.")
 
 	def post(self):
+		""" POST access to searching for recipes by their ingredients. """
 		args = self.reqparse.parse_args()
 
 		if user_able(args['key']):
-			t = None
+			recipes = None
 
 			if args['with'] == None:
 				args['with'] = []
@@ -134,118 +149,139 @@ class IngredientAPI(Resource):
 				args['without'] = []
 
 			for ing in args['with']:
-				q = db.session.query(models.Recipe).join(models.Ingredient, models.Recipe.ingredients).join(models.Ingredient_Name).filter(models.Ingredient_Name.name == ing)
-				t = q
-				print('With: ' + str(q.count()))
+				query = db.session.query(models.Recipe) \
+				          .join(models.Ingredient, models.Recipe.ingredients) \
+				          .join(models.Ingredient_Name) \
+				          .filter(models.Ingredient_Name.name == ing)
+				recipes = query
 
 			for ing in args['without']:
-				q = db.session.query(models.Recipe).join(models.Ingredient, models.Recipe.ingredients).join(models.Ingredient_Name).filter(models.Ingredient_Name.name == ing)
-				print('Without: ' + str(q.count()))
-				if t != None:
-					t = t.except_(q)
+				query = db.session.query(models.Recipe) \
+				          .join(models.Ingredient, models.Recipe.ingredients) \
+				          .join(models.Ingredient_Name) \
+				          .filter(models.Ingredient_Name.name == ing)
+
+				if recipes != None:
+					recipes = recipes.except_(query)
 				else:
-					t = q
+					recipes = query
 
-			rv = []
+			returned_recipes = []
 
-			for r in t:
-				rv.append(json_recipe(r))
+			for recipe in recipes:
+				returned_recipes.append(json_recipe(recipe))
 
-			return rv
+			return returned_recipes
 
 
 class RecipeAPI(Resource):
+	""" Gets a single recipe by its id.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
-		self.reqparse.add_argument('key', type=str, required=True, help='Your API Key is required.')
+		self.reqparse.add_argument('key', type=str, \
+			                       required=True, help='Your API Key is required.')
 
-	def get(self, id):
+	def get(self, recipe_id):
+		""" Get access for accessing a single recipe by its id. """
 		args = self.reqparse.parse_args()
 		if user_able(args['key']):
-			recipe = get_recipe(id)
+			recipe = get_recipe(recipe_id)
 
-			return(json_recipe(recipe))
+			return json_recipe(recipe)
 		else:
 			abort(403)
 
 class RecipeTitleAPI(Resource):
+	""" Allows full text search of recipes.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
-		self.reqparse.add_argument('key', type=str, required=True, help='Your API Key is required.')
-		self.reqparse.add_argument('name', type=str, required=True, help='Your search terms are required.')
+		self.reqparse.add_argument('key', type=str, \
+			                       required=True, help='Your API Key is required.')
+		self.reqparse.add_argument('name', type=str, \
+			                       required=True, help='Your search terms are required.')
 
 	def post(self):
+		""" POST access for full-text search of recipes. """
 		args = self.reqparse.parse_args()
 
 		if user_able(args['key']):
-			combined_search_vector = tsvector_concat(
-				models.Recipe.search_vector,
-				models.Ingredient.search_vector
+			combined_search_vector = tsvector_concat( \
+				models.Recipe.search_vector, \
+				models.Ingredient.search_vector \
 			)
 
-			recipes = (
-				db.session.query(models.Recipe)
-				.join(models.Ingredient, models.Recipe.ingredients)
-				.filter(
-					tsvector_match(
-						combined_search_vector,
-						to_tsquery(
-							'simple',
-							parse_search_query(args['name'])
-						),
-					)
-				)
+			recipes = ( \
+				db.session.query(models.Recipe) \
+				.join(models.Ingredient, models.Recipe.ingredients) \
+				.filter( \
+					tsvector_match( \
+						combined_search_vector, \
+						to_tsquery( \
+							'simple', \
+							parse_search_query(args['name']) \
+						), \
+					) \
+				) \
 			)
 
-			rv = []
+			returned_recipes = []
 
-			for r in recipes:
-				rv.append(json_recipe(r))
+			for recipe in recipes:
+				returned_recipes.append(json_recipe(recipe))
 
-			return(rv)
+			return returned_recipes
 		else:
 			abort(403)
 
 class RecipeAddAPI(Resource):
+	""" Adds a recipe to the database. As of right now, only allows admin
+	to add recipes.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('name', type=str, required=True)
 		self.reqparse.add_argument('description', type=str)
-		self.reqparse.add_argument('directions', type=str, action='append', required=True)
+		self.reqparse.add_argument('directions', type=str, \
+			                       action='append', required=True)
 		self.reqparse.add_argument('prep_time', type=str)
 		self.reqparse.add_argument('cook_time', type=str)
 		self.reqparse.add_argument('image', type=str)
-		self.reqparse.add_argument('ingredient', type=str, action='append', required=True)
+		self.reqparse.add_argument('ingredient', \
+			                       type=str, \
+			                       action='append', \
+			                       required=True)
 		self.reqparse.add_argument('key', type=str, required=True)
 		self.reqparse.add_argument('credit', type=str, required=True)
 
 	def post(self):
+		""" POST access for adding a recipe. """
 		args = self.reqparse.parse_args()
 
-		admin(args['key'])
+		is_admin(args['key'])
 
-		unknown_id = get_unknown()
-
-		recipe = models.Recipe(
-			name = args['name'],
-			description = args['description'],
-			prep_time = args['prep_time'],
-			cook_time = args['cook_time'],
-			image = args['image'],
-			credit = args['credit'])
+		recipe = models.Recipe( \
+			name=args['name'], \
+			description=args['description'], \
+			prep_time=args['prep_time'], \
+			cook_time=args['cook_time'], \
+			image=args['image'], \
+			credit=args['credit'])
 
 		db.session.add(recipe)
 
-		dirs = ''
+		directions = ''
 		if args['directions'] is not None:
-			for d in args['directions']:
-				dirs = dirs + '|' + d
+			for direction in args['directions']:
+				directions = directions + '|' + direction
 
-		recipe.directions = dirs
+		recipe.directions = directions
 
 		for i in args['ingredient']:
-			#ingredients are given in the form original/\amount/\unit/\name/\modifiers with
-			#modifiers being optional
+			#ingredients are given in the form
+			#original/\amount/\unit/\name/\modifiers
+			#with modifiers being optional
 
 			ing = i.split('/\\')
 			if len(ing) != 4 and len(ing) != 5:
@@ -253,21 +289,21 @@ class RecipeAddAPI(Resource):
 
 			ingredient = None
 			if len(ing) == 4:
-				n = add_ingredient_name(ing[3])
-				ingredient = models.Ingredient(
-					original = ing[0],
-					amount = ing[1],
-					unit = ing[2],
-					name = n)
+				ingredient_name = add_ingredient_name(ing[3])
+				ingredient = models.Ingredient( \
+					original=ing[0], \
+					amount=ing[1], \
+					unit=ing[2], \
+					name=ingredient_name)
 
 			if len(ing) == 5:
-				n =add_ingredient_name(ing[3])
-				ingredient = models.Ingredient(
-					original = ing[0],
-					amount = ing[1],
-					unit = ing[2],
-					name = n,
-					modifiers = ing[4])
+				ingredient_name = add_ingredient_name(ing[3])
+				ingredient = models.Ingredient( \
+					original=ing[0], \
+					amount=ing[1], \
+					unit=ing[2], \
+					name=ingredient_name, \
+					modifiers=ing[4])
 
 			db.session.add(ingredient)
 			recipe.ingredients.append(ingredient)
@@ -276,14 +312,17 @@ class RecipeAddAPI(Resource):
 		return "Recipe Added", 201
 
 class ResetFree(Resource):
+	""" Resets free credits. Admin access only.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('key', type=str, required=True)
 
 	def get(self):
+		""" GET access for resetting free credits. """
 		args = self.reqparse.parse_args()
 
-		admin(args['key'])
+		is_admin(args['key'])
 
 		users = db.session.query(models.User)
 
@@ -296,12 +335,16 @@ class ResetFree(Resource):
 
 
 class ReportError(Resource):
+	""" Allows people to report erronious recipes.
+	Does not consume API usage.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('id', type=str, required=True)
 		self.reqparse.add_argument('reason', type=str)
 
 	def post(self):
+		""" POST access for reporting errors in recipes. """
 		args = self.reqparse.parse_args()
 
 		body = "Recipe ID: " + args['id']
@@ -314,6 +357,9 @@ class ReportError(Resource):
 		return "Bad Recipe Reported", 200
 
 class AddIngredientToRecipe(Resource):
+	""" Adds an ingredient to a recipe. Reuses ingredient names,
+	if possible, but will create a new ingredient.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('key', type=str, required=True)
@@ -324,17 +370,18 @@ class AddIngredientToRecipe(Resource):
 		self.reqparse.add_argument('modifiers', type=str)
 
 	def post(self):
+		""" POST access for adding an ingredient to a recipe. """
 		args = self.reqparse.parse_args()
 
-		admin(args['key'])
+		is_admin(args['key'])
 
 		name = add_ingredient_name(args['name'])
-		ingredient = models.Ingredient(
-			original = 'Added and Edited from original recipe.',
-			amount = args['amount'],
-			unit = args['unit'],
-			name = name,
-			modifiers = args['modifiers'])
+		ingredient = models.Ingredient( \
+			original='Added and Edited from original recipe.', \
+			amount=args['amount'], \
+			unit=args['unit'], \
+			name=name, \
+			modifiers=args['modifiers'])
 
 		recipe = get_recipe(args['recipe_id'])
 		recipe.ingredients.append(ingredient)
@@ -344,6 +391,9 @@ class AddIngredientToRecipe(Resource):
 		return redirect('/recipe_edit/' + str(args['recipe_id']))
 
 class DeleteIngredientFromRecipe(Resource):
+	""" Removes an ingreidnet from a recipe. Only removes the link to
+	the recipe, but does not delete the ingredient from the database.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('key', type=str, required=True)
@@ -351,18 +401,25 @@ class DeleteIngredientFromRecipe(Resource):
 		self.reqparse.add_argument('ingredient_id', type=str, required=True)
 
 	def post(self):
+		""" POST access for removing ingredients from a recipe. """
+
 		args = self.reqparse.parse_args()
 
-		admin(args['key'])
+		is_admin(args['key'])
 
 		recipe = get_recipe(args['recipe_id'])
-		ingredient = db.session.query(models.Ingredient).filter(models.Ingredient.id == args['ingredient_id']).first()
+		ingredient = db.session.query(models.Ingredient) \
+		.filter(models.Ingredient.id == args['ingredient_id']).first()
 		recipe.ingredients.remove(ingredient)
 		db.session.commit()
 
 		return redirect('/recipe_edit/' + args['recipe_id'])
 
 class EditRecipe(Resource):
+	"""
+	Allows editing of the entire recipe except ingredients.
+	Ingredients are treated seperatly.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('name', type=str, required=True)
@@ -375,21 +432,27 @@ class EditRecipe(Resource):
 		self.reqparse.add_argument('origin', type=str, required=True)
 
 	def post(self):
+		""" POST access to edit the recipe. """
 		args = self.reqparse.parse_args()
 
-		admin(args['key'])
+		is_admin(args['key'])
 
 		recipe = get_recipe(args['recipe_id'])
-		recipe.directions=args['directions']
-		recipe.prep_time=args['prep_time']
-		recipe.cook_time=args['cook_time']
-		recipe.image_url=args['image_url']
-		recipe.origin=args['origin']
+		recipe.directions = args['directions']
+		recipe.prep_time = args['prep_time']
+		recipe.cook_time = args['cook_time']
+		recipe.image_url = args['image_url']
+		recipe.origin = args['origin']
 		db.session.commit()
 
 		return redirect('/recipe_edit/' + str(args['recipe_id']))
 
+
 class EditMealTime(Resource):
+	"""
+	This is the API access the edits what time of day (breakfast, lunch, etc)
+	that the meal is typically eaten.
+	"""
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('key', type=str, required=True)
@@ -397,9 +460,11 @@ class EditMealTime(Resource):
 		self.reqparse.add_argument('meal', type=int, required=True)
 
 	def post(self):
+		""" POST access to edit the meal. """
+
 		args = self.reqparse.parse_args()
 
-		admin(args['key'])
+		is_admin(args['key'])
 
 		recipe = get_recipe(args['id'])
 		recipe.meal = args['meal']
@@ -416,6 +481,7 @@ api.add_resource(ReportError, '/api/v1/report_error')
 
 #APIs for editing recipes.
 api.add_resource(AddIngredientToRecipe, '/api/v1/recipe_edit/add_ingredient')
-api.add_resource(DeleteIngredientFromRecipe, '/api/v1/recipe_edit/delete_ingredient')
+api.add_resource(DeleteIngredientFromRecipe,\
+	'/api/v1/recipe_edit/delete_ingredient')
 api.add_resource(EditRecipe, '/api/v1/recipe_edit/edit_recipe')
 api.add_resource(EditMealTime, '/api/v1/recipe_edit/edit_meal_time')
